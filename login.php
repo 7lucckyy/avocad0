22dddf8bd4dec644e9c27b1590043e3e66b1afa9
7lucckyy <7lucckyy6611@gmail.com>
2021-12-01 16:00:50 +0100

    Avocado BooksNG

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+include 'config.php';
+
+try{
+    
+        $email=stripcslashes($_POST['email']);
+        $email = mysqli_real_escape_string($conn, $email);
+        $password=$_POST['password'];
+        $password = mysqli_real_escape_string($conn, $password);
+
+        $sql = "SELECT * FROM users WHERE email = '$email' ";  
+        $result = mysqli_query($conn, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);
+
+    
+        if($email == $row['email']){
+        
+        if (password_verify($password, $row['password'])) {
+            header("Content-Type: application/json");
+            http_response_code(200);
+            mysqli_query($conn, $sql);
+            $message = json_encode(array("message" => "Login Success", "status" => true));	
+            echo $message;
+            exit();  
+        } else {
+            header("Content-Type: application/json");
+            http_response_code(401);
+            mysqli_query($conn, $sql);
+            $message = json_encode(array("message" => "Invalid Password ","status" => false));	
+            echo $message;
+            exit();
+        }
+    }
+            
+    }
+    catch(Exception $e) {
+        http_response_code(401);
+        echo('Message: ' .$e->getMessage());
+        $message = json_encode(array("message" => $e->getMessage(),"status" => false));	
+        echo $message;
+      }
+    
+    
+
+?>
